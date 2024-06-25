4e3b0efa26ba558a7f461da9227cb3cfd41ac46e
Bonnement Léo <leobonnement@gmail.com>
2024-06-25 16:20:03 +0200

    Avancement front

--- app/src/main/java/com/example/jeucalculmental/MainActivity.java
+++ app/src/main/java/com/example/jeucalculmental/MainActivity.java
@@ -1,25 +1,40 @@
 package com.example.jeucalculmental;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnStartGame;
+    private Button btnViewHighScore;
+    private Button btnAbout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void startGame(View view) {
-        Intent intent = new Intent(this, GameActivity.class);
-        startActivity(intent);
-    }
+        btnStartGame = findViewById(R.id.btnStartGame);
+        btnViewHighScore = findViewById(R.id.btnHighScore);
+        btnAbout = findViewById(R.id.btnAbout);
 
-    public void viewHighScore(View view) {
-        Intent intent = new Intent(this, HighScoreActivity.class);
-        startActivity(intent);
+        btnStartGame.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            startActivity(intent);
+        });
+
+        btnViewHighScore.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, HighScoreActivity.class);
+            startActivity(intent);
+        });
+
+        btnAbout.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+        });
     }
 }
